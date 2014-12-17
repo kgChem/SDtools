4e9f2677dfb51a86e69198c21e88d2d363a03fa6
kgChem <kevin.greenman@gmail.com>
2014-12-16 16:25:45 -0800

    Add file iterator

--- sdreader.py
+++ sdreader.py
@@ -2,13 +2,38 @@
 #
 class SDreader(object):
     def __init__(self,filename):
-        self.file = filename
         try:
-            f = open(filename)
+            self.f = open(filename,"rb")
         except IOError as e:
             print("ERROR! Could not open the file {0}".format(filename))
-            self.file = None
+            self.f = None
         else:
-            f.close()
+            self.filename = filename
+            self.pos = 0
+            self.numRecords = 0
+            self.fields = []
+            self.records = []
+
+    def __iter__(self):
+        return(self)
+
+    #Returns a block of text corresponding to 1 record in the SD File
+    def next(self):
+        lines = []
+        linebuffer = self.f.readline()
+        while (linebuffer and not ('$$$$' in linebuffer)):
+            lines.append(linebuffer)
+            linebuffer = self.f.readline()
+        lines.append(linebuffer)
+        return(''.join(lines))
             
         
+testfile = "E:/Documents/Computational/ChemInformatics/ChelatingFragments.sdf"
+#S = SDreader("banana stand")
+S = SDreader(testfile)
+print('~'*80)
+S0 = S.next()
+print(S0)
+print('~'*80)
+S1 = S.next()
+print(S1)
